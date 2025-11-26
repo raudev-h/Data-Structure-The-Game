7f4731950c5981f4f29ef495b8713a668a422b4d
raul <raulalbertohechavarria@gmail.com>
2025-11-25 22:09:39 -0500

    feat:add resources classes

---
+++ graph-dominion/src/main/java/dominion/model/resources/ResourceCollection.java
@@ -0,0 +1,9 @@
+package dominion.model.resources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceCollection {
+private final Map<ResourceType,Integer> resources = new HashMap<>();
+
+}
