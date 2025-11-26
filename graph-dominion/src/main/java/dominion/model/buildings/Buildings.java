c474759137aa759d419f9221973411202c26e463
raul <raulalbertohechavarria@gmail.com>
2025-11-25 22:13:56 -0500

    feat:add buildings class and territory class

---
+++ graph-dominion/src/main/java/dominion/model/buildings/Buildings.java
@@ -0,0 +1,9 @@
+package dominion.model.buildings;
+
+import dominion.model.territories.Territory;
+
+public abstract class Buildings {
+    protected String id;
+    protected Territory territory;
+    protected int currentHealth;
+}
