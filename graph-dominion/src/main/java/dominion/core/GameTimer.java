a2d00cdcfaae45c2f248d165c2df73810e278ca8
Aniel.py <anielvarela64@gmail.com>
2025-11-26 06:34:47 -0500

    Se agrega el constructor de GameTimer

--- graph-dominion/src/main/java/dominion/core/GameTimer.java
+++ graph-dominion/src/main/java/dominion/core/GameTimer.java
@@ -2,7 +2,17 @@ package main.java.dominion.core;
 
 public class GameTimer {
 
+    //FIELDS
     private long TotalGameTime;
     private boolean isRunning;
     private long pausedTime;
+
+    //CONSTRUCTOR
+    public GameTimer(){
+        TotalGameTime = 0;
+        isRunning = false;
+        pausedTime  = 0;
+    }
+
 }
+
