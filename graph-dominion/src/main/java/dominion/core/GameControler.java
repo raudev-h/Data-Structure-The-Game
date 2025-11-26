81d2ca03c7c951728a66b9d65533a7e280c917c4
Aniel.py <anielvarela64@gmail.com>
2025-11-26 07:43:18 -0500

    feat: Agrego de funcion de start, pauser y getElapsedSecons, en GameTimer y GameController

--- graph-dominion/src/main/java/dominion/core/GameControler.java
+++ graph-dominion/src/main/java/dominion/core/GameControler.java
@@ -6,10 +6,29 @@ import java.util.ArrayList;
 
 public class GameControler {
 
+    //FIELDS
+
     private GameTimer gametimer;
     private GameMap gameMap;
     private ArrayList<Player> players;
     private boolean gameRunning;
 
+    //TODO:CONSTRUCTOR
+
+    //METHODS
+    
+    //Clock Methods
+
+    public boolean pauseClock(){
+        return gametimer.pause();
+    }
+
+    public boolean startClock(){
+        return gametimer.start();
+    }
+
+    public long getElapsedSeconds(){
+        return gametimer.getElapsedSeconds();
+    }
 
 }
--- graph-dominion/src/main/java/dominion/core/GameTimer.java
+++ graph-dominion/src/main/java/dominion/core/GameTimer.java
@@ -3,16 +3,52 @@ package main.java.dominion.core;
 public class GameTimer {
 
     //FIELDS
-    private long TotalGameTime;
+
+    private long totalGameTime;
     private boolean isRunning;
-    private long pausedTime;
+    private long startTime;
 
     //CONSTRUCTOR
+
     public GameTimer(){
-        TotalGameTime = 0;
+        totalGameTime = 0;
         isRunning = false;
-        pausedTime  = 0;
+        startTime  = 0;
     }
+
+    //METHODS
+
+    //Marcar el marcador en el inicio de juego o despues de una pausa
+    public boolean start(){
+        if(!isRunning){
+            isRunning = true;
+            startTime = System.currentTimeMillis();
+        }
+        return isRunning;
+    }
+
+    /* Devolver el tiempo transcurridp desde que el usuario comenzó el juego o salió
+    de la pantalla de pausa + el tiempo juegado anteriormente */
+    public long getElapsedSeconds(){
+        long t = totalGameTime;
+        if(isRunning){
+            t += System.currentTimeMillis() - startTime;
+
+        }
+        return t/1_000;
+    }
+
+    //Pausar el juego y agregar el tiempo jugado transcurrido al total
+    public boolean pause(){
+        boolean pausado = false;
+        if(isRunning){
+            pausado = true;
+            isRunning = false;
+            totalGameTime += System.currentTimeMillis() - startTime;
+        }
+        return pausado;
+    }
+
 
 }
 
