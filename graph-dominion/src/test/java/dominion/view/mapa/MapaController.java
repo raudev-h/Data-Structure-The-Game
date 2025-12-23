3adf6083f03cfda11be0043ecd760aaf9ccf16ab
s0sa26 <guevarasosacarlos@gmail.com>
2025-12-23 16:11:25 -0500

    interfaz de prueba

---
+++ graph-dominion/src/test/java/dominion/view/mapa/MapaController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mapa;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class MapaController implements Initializable {
+
+    @FXML
+    private Button backToMenu;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    public void backToMenuAction(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu/interfaz.fxml"));
+            Scene newScene = new Scene(loader.load());
+            
+            Stage newStage = new Stage();
+            newStage.setTitle("Menu");
+            newStage.setScene(newScene);
+            newStage.show();
+            
+           Stage menuStage = (Stage) backToMenu.getScene().getWindow();
+           menuStage.close();
+            
+            
+            
+            
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+        
+    
+}
