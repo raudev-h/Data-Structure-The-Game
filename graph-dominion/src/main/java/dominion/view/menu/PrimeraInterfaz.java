c32eb1805f49ffd36e3afd05511106ffe7fa3d41
raul <raulalbertohechavarria@gmail.com>
2025-12-24 17:17:54 -0500

    fix PrimeraInterfaz class

--- graph-dominion/src/main/java/dominion/view/menu/PrimeraInterfaz.java
+++ graph-dominion/src/main/java/dominion/view/menu/PrimeraInterfaz.java
@@ -1,4 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package dominion.view.menu;
 
-public class PrimeraInterfaz {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrimeraInterfaz extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("interfaz.fxml"));
+
+        primaryStage.setTitle("Menu");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+
+    }
+
+
 }
+
