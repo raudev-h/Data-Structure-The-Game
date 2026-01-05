5542ddf640ae8eaba45b9d0c00bf6449d3005742
s0sa26 <guevarasosacarlos@gmail.com>
2026-01-04 20:44:45 -0500

    la interfaz solo en archivos(probar)

--- graph-dominion/src/main/java/dominion/view/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.juego.conquista {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
-
-    opens com.juego.conquista to javafx.fxml;
-    exports com.juego.conquista;
-}
